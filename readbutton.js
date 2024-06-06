4b4c9bbbca9c7b12ce82114fbba9e42c5549924d
Jhomic <az.jhomic@gmail.com>
2024-06-06 19:01:39 -0300

    ads_theater

--- readbutton.js
+++ readbutton.js
@@ -1,12 +1,6 @@
-const hamburger =  document.querySelector(".hamburger");
-const nav = document.querySelector(".nav");
-
 const readbutton = document.querySelector(".readbutton")
 const fullarticle = document.querySelector(".fullarticle")
 const buttonclose = document.querySelector(".fullarticle button")
-
-hamburger.addEventListener("click", () => 
-nav.classList.toggle("active"));
 
 readbutton.onclick = function () {
     fullarticle.showModal()
